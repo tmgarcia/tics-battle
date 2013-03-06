d6f92e8d9f52918f8b30b9a8bc12a34ebc5797a3
Lincoln Grow <daxetan1@gmail.com>
2013-03-06 11:22:49 -0700

    added test fire method

--- testjavascript.js
+++ testjavascript.js
@@ -1,3 +1,9 @@
 function makeBlue(){
   document.getElementById("testText").style.color="blue";
 }
+function fire(coordinates) {
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST", "http://dickerson.neumont.edu:8080/Battleship/GameRequest/Fire", false);
+	xmlhttp.send("<request><coordinates>" + coordinates + "</coordinates></request>");
+	return xmlhttp.responseXML;
+}
