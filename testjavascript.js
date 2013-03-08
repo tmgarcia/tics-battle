01ace269f3e1f363b8249c3844c493703457e072
Lincoln Grow <daxetan1@gmail.com>
2013-03-08 11:06:57 -0700

    changed some stuff in testhtml and testjavascript

--- testjavascript.js
+++ testjavascript.js
@@ -1,9 +1,15 @@
 function makeBlue(){
   document.getElementById("testText").style.color="blue";
 }
-function fire(coordinates) {
+function fire() {
 	xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("POST", "http://dickerson.neumont.edu:8080/Battleship/GameRequest/Fire", false);
-	xmlhttp.send("<request><coordinates>" + coordinates + "</coordinates></request>");
-	return xmlhttp.responseXML;
+	xmlhttp.send("<request><coordinates>" + document.getElementById("coords").value + "</coordinates></request>");
+	document.getElementById("fireResults").innerHTML = xmlhttp.responsetext;
+	var response = xmlhttp.responseXML;
+	
+}
+function fireResults(id) {
+	var results = document.getElementById(id).value;
+	return results;
 }
