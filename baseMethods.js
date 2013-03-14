1551ad438be98a6829f6c2afee00f32148e2c721
Jeremy Jones <jeremy29229@yahoo.com>
2013-03-14 10:43:33 -0600

    Put all remaining javascript into baseMethods and updated corresponding html docs.

--- baseMethods.js
+++ baseMethods.js
@@ -284,3 +284,114 @@ function displayShip(ship, dir, cell){
 		}
 	}
 }
+
+/*---------Join A Game Methods---------*/
+var thename; //This is the joining players name
+var x; // Holds the all <game> response xml
+
+function sendID(name)
+{  
+	if(name === "")
+	{
+		document.getElementById("table").innerHTML = "You must enter a name with at least one character.";
+	}
+	else
+	{
+		thename = name;
+		document.getElementById("playerForm").innerHTML = "<h2 id='welcome'>Welcome " + thename + ".</h2>";
+		makeTable();
+	}
+}
+
+function makeTable()
+{
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.withCredtials = true;
+	xmlhttp.open("POST", "http://dickerson.neumont.edu:8080/Battleship/GameRequest/GameList", false);
+	xmlhttp.send("<request></request>");
+	xmlDoc = xmlhttp.responseXML; 
+	
+	var w = "<h1>Here is a list of available games:</h1>";
+	w =  w + "<table border='1'>";
+	
+	w = w + "<th>Game ID</th><th>Player's Name</th><th>Join Game</th>";
+	
+	x = xmlDoc.getElementsByTagName("game");
+	for (i = 0; i < x.length; i++)
+	{
+		w = w + "<tr><td>";
+		w = w + x[i].getElementsByTagName("gameID")[0].childNodes[0].nodeValue;
+		w = w + "</td><td>";
+		w = w + x[i].getElementsByTagName("turn")[0].childNodes[0].nodeValue;
+		w = w + "</td><td>";
+		w = w + "<button id =" + i + " onclick='joinGame(this.id)'>Join</button>";
+		w = w + "</td></tr>";
+	}
+	w = w + "</table>";
+	document.getElementById("table").innerHTML= w;
+}
+
+function joinGame(clicked_ID)
+{
+
+	var selectedGame = x[clicked_ID].getElementsByTagName("gameID")[0].childNodes[0].nodeValue;
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.withCredtials = true;
+	xmlhttp.open("POST", "http://dickerson.neumont.edu:8080/Battleship/GameRequest/Join", false);
+	xmlhttp.send("<request><playerID>" + thename + "</playerID><gameID>" + selectedGame + "</gameID></request>");
+	xmlDoc = xmlhttp.responseXML;
+	
+	var a = xmlDoc.getElementsByTagName("response");
+	var b = "<ul><li>";
+	b = b + a[0].getElementsByTagName("gameID")[0].childNodes[0].nodeValue;
+	b = b + "</li><li>";
+	b = b + a[0].getElementsByTagName("result")[0].childNodes[0].nodeValue;
+	b = b + "</li></ul>";
+	
+	document.getElementById("table").innerHTML = b;
+	window.setTimeout(window.open('PlaceShips.html','_self','','true'), 10000);
+}
+
+/*---------Create An AI Game Methods---------*/
+var playerName;
+var botName;
+var z;
+
+function sendID_AI(name)
+{  
+  if(name === "")
+	{
+		document.getElementById("bots").innerHTML = "You must enter a name with at least one character.";
+	}
+	else
+	{
+		playerName = name;
+		document.getElementById("playerForm").innerHTML = "<h2 id='welcome'>Welcome " + playerName + ".</h2>";
+		showBots();
+	}
+}
+
+function showBots()
+{
+		z = "<h2><a onclick='startBotGame(&#39;Edison&#39;)'>Play Edison (Hard)</a></h2>";
+		z = z + "<h2><a onclick='startBotGame(&#39;Geeves&#39;)'>Play Geeves (Medium)</a></h2>";
+		z = z + "<h2><a onclick='startBotGame(&#39;Robby&#39;)'>Play Robby (Easy)</a></h2>";
+		document.getElementById("bots").innerHTML = z;
+}
+
+function startBotGame(botName)
+{
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.withCredtials = true;
+	xmlhttp.open("POST", "http://dickerson.neumont.edu:8080/Battleship/GameRequest/NewGame", false);
+	xmlhttp.send("<request><playerID>" + playerName + "</playerID><robot>" + botName + "</robot></request>");
+	xmlDoc = xmlhttp.responseXML;
+	
+	var a = xmlDoc.getElementsByTagName("response");
+	var b = "<ul><li>";
+	b = b + a[0].getElementsByTagName("gameID")[0].childNodes[0].nodeValue;
+	b = b + "</li></ul>";
+	
+	document.getElementById("bots").innerHTML = b;
+	window.setTimeout(window.open('PlaceShips.html','_self','','true'), 10000);
+}
